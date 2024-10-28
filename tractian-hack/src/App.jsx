6782acd90a506f2b69ca538413d1a884e5e15e27
iTzFabregas <fabricio-sampaio@usp.br>
2024-10-28 14:01:54 -0300

    feat: deletinf solicatacao

--- tractian-hack/src/App.jsx
+++ tractian-hack/src/App.jsx
@@ -19,7 +19,8 @@ function App() {
                 <div className='w-1/2 flex flex-col items-start justify-center p-16 gap-20'>
 					<div className= "gap-10">
 						<h1 className="text-8xl font-bold mb-4 relative z-10 font-poppins"> Automatizamos Ordem de Serviços </h1>
-						<p className="font-jetbrains text-2xl mb-4 relative z-10"> michel texto texto texto textoasjdad asodjasd a maosda daajsdas dad aoisdjasd jaiosdjaiosd asdiasmd micehalca iajc aec</p>
+						<p className="font-jetbrains text-2xl mb-4 relative z-10"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, nobis. Nulla, dolorum molestias beatae itaque aperiam nostrum totam eius ipsum pariatur ducimus minima harum magnam enim iusto, voluptatibus id. Nemo aliquid magni repellendus aperiam non placeat aut. Dolorem sint sed debitis nisi corporis ut fugiat, earum hic unde nihil aut exercitationem reprehenderit officiis? Rerum voluptas ipsum placeat, recusandae quam quidem in, aspernatur itaque quos commodi perspiciatis consectetur voluptate quia, corporis officiis beatae incidunt magni! Placeat culpa eligendi enim quidem tempore officiis, rerum ex odit aspernatur qui mollitia, voluptatem fugiat laudantium ipsam impedit velit? Voluptas doloribus ipsam consequuntur quo, aliquid praesentium?
+						</p>
 					</div> 
 					<div className='flex flex-row gap-20 font-bold text-4xl'>
 						<button className='font-poppins text-white border-solid border-2 border-white bg-trac-blue p-4 rounded-base w-72 text-center hover:bg-gray-800 transition duration-300' onClick={(e) => handleClick(e)}>Manutentor</button>
--- tractian-hack/src/components/Assistente.jsx
+++ tractian-hack/src/components/Assistente.jsx
@@ -8,6 +8,16 @@ export default function Assistente() {
     const [status, setStatus] = useState('idle') // 'idle', 'ready'
     const [text, setText] = useState('')
 
+    const [items, setItems] = useState([
+        { id: 1, text: 'Item 1' },
+        { id: 2, text: 'Item 2' },
+        { id: 3, text: 'Item 3' },
+      ]);
+
+    const deleteItem = (id) => {
+        setItems(items.filter(item => item.id !== id));
+    };
+
     const handleClick = (e) => {
 		setStatus('ready')
 		
@@ -17,30 +27,29 @@ export default function Assistente() {
         <div className="w-full h-screen flex p-12 bg-gray-200">
             <div className="flex flex-col items-center w-8/12 bg-gray-200 text-center">
                 {status === 'idle' && <button className="bg-sky-500 p-2 w-2/5 h-14 rounded-xl text-xl font-bold hover:bg-blue-600 transition duration-300 text-white" onClick={(e) => handleClick(e)}>Criar uma nova ordem de serviço</button>}
+
                 {status === 'ready' && 
                 <div className="flex w-8/12 gap-10 items-center">
-                    <div className='flex w-full'>
-                        <input
-                        type="text"
-                        value={text}
-                        onChange={(e) => setText(e.target.value)}
-                        placeholder="Digite ou fale aqui..."
-                        className="w-full p-2 border border-gray-300 rounded-md pr-16 h-10"
-                        />
-                        <button className="h-10/12 p-2 mr-10 w-24 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">
-                        Falar
-                        </button>
+                    <div className='flex w-full gap-1'>
+                        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Digite ou fale aqui..." className="w-full p-2 border border-gray-300 rounded-md pr-16 h-10" />
+                        <button className="h-10/12 p-2 mr-10 w-24 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"> Falar </button>
                     </div>
-                    <button className="h-10/12 p-2 mr-10 w-24 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">
-                        Enviar
+                    <button className="h-10/12 p-2 w-72 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">
+                        Gerar ordem de serviço
                     </button>
                 </div>}
             </div>
             <div className="w-4/12 bg-gray-500 overflow-y-auto">
-                    <h1 className="text-center text-xl bg-gray-600 m-3 rounded-xl p-1">Solicitações de manutenção</h1>
-                    <div className="m-5 bg-white h-1/5 rounded-xl text-black">
-                    <h2>Estado: CRÍTICO</h2> 
-                </div>
+                <h1 className="text-center text-xl bg-gray-600 m-3 rounded-xl p-1">Solicitações de manutenção</h1>
+                {items.map(item => (
+                    <div className="m-5 bg-white h-1/5 rounded-xl text-black">  
+                        <div className="text-right pt-4 pr-4 font-bold text-red-500 text-2xl">
+                            <button className="" onClick={() => deleteItem(item.id)}>X</button>
+                        </div>
+                        <h2>Estado: CRÍTICO</h2>
+                        <h2>Estado: CRÍTICO</h2>
+                    </div>
+                ))}
             </div>
         </div>
     )
