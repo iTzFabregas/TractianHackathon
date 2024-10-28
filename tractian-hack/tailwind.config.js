e5c24af5fff578d044f6756a60caf97877441bf6
MichelH7cker <michel.hecker@usp.br>
2024-10-28 13:35:42 -0300

    style landing page

--- tractian-hack/tailwind.config.js
+++ tractian-hack/tailwind.config.js
@@ -1,21 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        '01': "url('/01.png')",
-        '03': "url('/03.jpg')"
-      },
-      from: {
-        '01': "url('/01.png')",
-        '03': "url('/03.jpg')"
-      },
-      padding: {
-        '2.5': "10.5px"
-      }
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'tower': "url('/tower.avif')", // Define uma imagem de fundo personalizada
+            },
+            from: {
+                'tower': "url('/tower.avif')",
+            },
+            padding: {
+                '2.5': "10.5px", // Define um novo valor de padding
+            },
+            colors: {
+                'trac-blue': '#025ef4',  // Define sua cor personalizada
+            },
+            fontFamily: {
+                'poppins': ['Poppins', 'sans-serif'], 
+                'jetbrains': ['JetBrains Mono', 'monospace'], // Adiciona a família de fontes JetBrains Mono
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
